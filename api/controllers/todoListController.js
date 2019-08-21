5a5a2c4fe4285021d1b37978ec96c8f69b11f888
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-08-21 15:15:54 -0700

    finished the task controller

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -13,5 +13,35 @@ exports.list_all_tasks = function(req, res) {
 
 exports.create_a_task = function(req, res) {
 	var new_task = newTask(req.body);
-	new_task
-}
+	new_task.save(function(err, task) {
+		if(err)
+			res.send(err);
+		res.json(task);
+	});
+};
+
+exports.read_a_task = function(req, res) {
+	Task.findById(req.params.taskId, function(err, task) {
+		if(err)
+			res.send(err);
+		res.json(task);
+	});
+};
+
+exports.update_a_task = function(req, res) {
+	Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+		if (err)
+			res.send(err);
+		res.json(task);
+	});
+};
+
+exports.delete_a_task = function(req, res) {
+	Task.remove({
+		_id: req.params.taskId
+	}, function(err, task) {
+		if (err)
+			res.send(err);
+		res.json({ message: 'Task successfully deleted' });
+	});
+};
